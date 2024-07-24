365e259e3bb96b8642f2b3d3a862345d98c74660
oncomouse <oncomouse@gmail.com>
2024-07-24 11:38:20 -0500

    feat: better :LazyGit command

--- lua/neovim-config/commands.lua
+++ lua/neovim-config/commands.lua
@@ -50,7 +50,25 @@ function M.setup()
 		nargs = 1,
 	})
 
-	vim.api.nvim_create_user_command("LazyGit", ":terminal lazygit<cr>", {
+	vim.api.nvim_create_user_command("LazyGit", function()
+		if vim.fn.executable("lazygit") == 1 then
+			vim.cmd([[term lazygit]])
+			vim.keymap.set("n", "q", function()
+				vim.cmd([[quit]])
+			end, {
+				buffer = true,
+			})
+			vim.api.nvim_feedkeys("i", "n", false)
+			vim.api.nvim_create_autocmd("TermClose", {
+				buffer = vim.fn.bufnr(""),
+				command = "if !v:event.status | exe 'bdelete! '..expand('<abuf>') | endif",
+			})
+		else
+			vim.cmd([[echohl ErrorMsg
+		echo 'lazygit was not found in $PATH'
+		echohl NONE]])
+		end
+	end, {
 		force = true,
 		desc = "Load lazygit in a terminal.",
 	})
@@ -60,8 +78,7 @@ function M.setup()
 		vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
 		vim.cmd("e " .. file)
 	end, {
-		desc =
-		"Implement the functionality of mf: if directory does not exist for new file create it before creating the file.",
+		desc = "Implement the functionality of mf: if directory does not exist for new file create it before creating the file.",
 		complete = "dir",
 		nargs = 1,
 	})
