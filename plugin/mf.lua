55ae5a9f68e49ab9d0fe5d5d497f4fa8f0f83c86
oncomouse <oncomouse@gmail.com>
2024-07-24 11:11:53 -0500

    fix: update deprecated object name

--- plugin/mf.lua
+++ plugin/mf.lua
@@ -1,6 +1,6 @@
 -- Mf for directory creation:
 vim.api.nvim_create_user_command("Mf", function(args)
-	local file = vim.loop.fs_realpath(args.args) or args.args
+	local file = vim.uv.fs_realpath(args.args) or args.args
 	vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
 	vim.cmd("e " .. file)
 end, {
