9f20d879d4b88ccf2786e4f20afb02c2080cb3fb
oncomouse <oncomouse@gmail.com>
2026-07-04 12:58:42 -0500

    feat(lsp): disable linting and formatting in CSS and HTML LSPs

---
+++ after/lsp/cssls.lua
@@ -0,0 +1,8 @@
+return {
+	init_options = { provideFormatter = false }, -- needed to enable formatting capabilities
+	settings = {
+		css = { validate = false },
+		scss = { validate = false },
+		less = { validate = false },
+	},
+}
